ef2b1074357dfc686ed9b6cdc7a1923f4aa75a5b
Bryce Jech <bryce@brycejech.com>
2019-05-20 16:52:47 -0500

    require exercise modules

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const repeat        = require('./exercises/basic/repeat-a-string'),
+      reverse       = require('./exercises/basic/reverse-a-string'),
+      factorialize  = require('./exercises/basic/factorialize'),
+      confirmEnding = require('./exercises/basic/confirm-ending'),
+      booWho        = require('./exercises/basic/boo-who');
+
+console.log(repeat);
+console.log(reverse);
+console.log(factorialize);
+console.log(confirmEnding);
+console.log(booWho);
