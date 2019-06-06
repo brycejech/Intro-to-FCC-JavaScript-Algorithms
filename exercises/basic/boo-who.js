329cb00819e2015812e19bd98ad3773e287cecf5
Bryce Jech <bryce@brycejech.com>
2019-06-05 20:31:13 -0500

    add solution comments

--- exercises/basic/boo-who.js
+++ exercises/basic/boo-who.js
@@ -1,13 +1,49 @@
 'use strict';
 
+/*
+    typeof Solution
+    ---------------
+
+    Use the 'typeof' operator to check if bool is a boolean and return the result
+*/
 function booWho(bool){
     return typeof bool === 'boolean';
 }
 
+/*
+    Object.prototype Solution
+    -------------------------
+
+    Use the Object.prototype.toString method to determine the type
+
+    This solution is more reliable in almost all cases for determining the true
+    type of an object. However, it is a bit overkill for checking if a thing is
+    a boolean.
+
+    Here are some examples where 'typeof' can give incorrect results
+        - typeof null // object
+        - typeof NaN  // number
+        - typeof []   // object
+
+    Object.prototype.toString should also be favored when you may want to know
+    more specifically what type a 'thing' is
+        - Object.prototype.toString.call(new Date)) // '[object Date]'
+        - Object.prototype.toString.call(/abc/)     // '[object RegExp]'
+        - Object.prototype.toString.call(null)      // '[object Null]'
+        - Object.prototype.toString.call(undefined) // '[object Undefined]'
+*/
 function booWho2(bool){
     return Object.prototype.toString.call(bool) === '[object Boolean]';
 }
 
+/*
+    _type Helper Solution
+    ---------------------
+
+    Use the _type helper to determine if a thing is a boolean
+
+    This solution is equally as overkill as solution 2 for this given problem
+*/
 function booWho3(bool){
     return _type(bool) === 'boolean';
 }
