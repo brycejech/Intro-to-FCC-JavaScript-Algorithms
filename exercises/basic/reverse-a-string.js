c1d639ed5f1f0475a77ba8cee5a21dc9c5da59e3
Bryce Jech <bryce@brycejech.com>
2019-06-06 18:55:01 -0500

    add solution notes

--- exercises/basic/reverse-a-string.js
+++ exercises/basic/reverse-a-string.js
@@ -1,23 +1,47 @@
 'use strict';
 
-// One-liner
+/*
+    Array Solution
+    --------------
+
+    1. Split the string into single characters
+    2. Reverse the array
+    3. Join the array back on empty string
+    4. Return the result
+
+    This is the most common and recommended approach to solving this problem
+*/
 const reverse = str => str.split('').reverse().join('');
 
-// Reverse iteration
+/*
+    For-loop Solution
+    -----------------
+
+    1. Initialize an empty string that will be returned
+    2. Loop over each character of the string from right to left
+        2a. Concatenate the character onto reversed
+    3. Return the result
+*/
 function reverse2(str){
     let reversed = '';
 
-    // Concat onto "reversed" by walking the string back to front
-    for(let i = str.length -1; i >= 0; i--) reversed += str[i];
+    for(let i = str.length - 1; i >= 0; i--) reversed += str[i];
 
     return reversed;
 }
 
-// Prepending solution
+/*
+    For-of Solution
+    ---------------
+
+    1. Initialize an empty string that will be returned
+    2. Split the string on empty string and loop over each character
+        2a. Using a template literal, prepend the character onto reversed
+    3. Return the result
+*/
 function reverse3(str){
     let reversed = '';
 
-    // Walk the string front to back and prepend each char to "reversed"
     for(const char of str.split('')) reversed = `${ char }${ reversed }`;
 
     return reversed;
